e43c22ee7b3db73da243bd8d7bc336bc9d283851
Tobe Kirchner <kirchner.to@gmail.com>
2015-05-18 16:33:26 +0200

    made firebase and angularfire to use locally and added user-binding to rootScope

--- walkieDoggie/www/js/controllers.js
+++ walkieDoggie/www/js/controllers.js
@@ -2,16 +2,11 @@ angular.module('starter.controllers', [])
 
 .controller('AuthCtrl', function($scope, $state, $timeout, Auth) {
 
-  $scope.user = {
-    email: '',
-    password: '',
-    vorname: '',
-    nachname: ''
-  }
+  $scope.auth = {};
 
   // perfrom the login action when the user submits the login form
   $scope.doLogin = function() {
-    Auth.login($scope.user).then(function(authData) {
+    Auth.login($scope.auth).then(function(authData) {
       console.log("Nutzer " + authData.uid + " wurde eingeloggt");
       $state.go('tab.feed.alle');
       console.log("Weiterleitung auf den Feed");
@@ -23,6 +18,7 @@ angular.module('starter.controllers', [])
   $scope.doFacebookConnect = function() {
     Auth.facebook()
     .then(function(authData) {
+      $state.go('tab.feed.alle');
       console.log(authData);
     }).catch(function(error) {
       console.log("Authentication failed:", error);
@@ -31,8 +27,8 @@ angular.module('starter.controllers', [])
 
   // perform the register action when the user submits the register form
   $scope.doRegister = function() {
-    Auth.register($scope.user).then(function(userData) {
-      Auth.createProfile($scope.user, userData);
+    Auth.register($scope.auth).then(function(userData) {
+      Auth.createProfile($scope.auth, userData);
       console.log("Nutzer " + userData.uid + " wurde registriert");
       return $scope.doLogin();
     });
@@ -53,6 +49,6 @@ angular.module('starter.controllers', [])
 	$scope.mitglied = Mitglieder.get($stateParams.mitgliedId);
 })
 
-.controller('ProfilCtrl', function($scope, User){
-  User.getUser().$bindTo($scope, "user");
+.controller('ProfilCtrl', function($rootScope){
+
 })
